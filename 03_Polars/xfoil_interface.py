5bec2ceacf354812240b290d04bcbc563416650e
Norberto Goizueta <ng213@ic.ac.uk>
2021-11-28 21:34:12 +0000

    Support naca airfoils

--- 03_Polars/xfoil_interface.py
+++ 03_Polars/xfoil_interface.py
@@ -23,11 +23,11 @@ def read_excel(filepath):
 
     out = {}
     outname = ['root', 'tip']
-    for isheet, sn in enumerate([6]):
+    for isheet, sn in enumerate([6, 7]):
         df = pd.read_excel(filepath, sheet_name=sn, header=2, usecols=[1, 2, 3, 4])
 
-        x_upper, y_upper = interpolate_airfoil_surface(df['XU'], df['YU'], n_points=300)
-        x_lower, y_lower = interpolate_airfoil_surface(df['XL'], df['YL'], n_points=300)
+        x_upper, y_upper = interpolate_airfoil_surface(df['XU'], df['YU'], n_points=200)
+        x_lower, y_lower = interpolate_airfoil_surface(df['XL'], df['YL'], n_points=200)
 
         x = np.concatenate((x_lower[::-1], x_upper))
         y = np.concatenate((y_lower[::-1], y_upper))
@@ -41,7 +41,7 @@ def read_excel(filepath):
 def interpolate_airfoil_surface(x_airfoil, y_airfoil, n_points=100):
     """Upper or lower surface only"""
     x_chord = np.linspace(np.min(x_airfoil), np.max(x_airfoil), n_points)
-
+    x_chord = - np.cos(x_chord ** 2 * np.pi) * 0.5 + 0.5
     order = np.argsort(x_airfoil)
     x_airfoil = x_airfoil[order]
     y_airfoil = y_airfoil[order]
@@ -56,7 +56,7 @@ class XFoilInterface:
         self.xfoil = XFoil()
         self.configuration = configuration
 
-        self.airfoil_data = None  # dict: containing coords at 'root' and 'tip'
+        self.airfoil_data = dict() # dict: containing coords as {airfoil_name: (x, y)}
 
         self._reynolds = None
 
@@ -73,7 +73,7 @@ class XFoilInterface:
         self._reynolds = reynolds_number
         self.xfoil.Re = reynolds_number
 
-    def load_airfoils(self):
+    def load_airfoils_from_excel(self):
 
         if self.configuration.lower() == 'reference':
             filepath = flex_op_data_path + '/Reference.xlsx'
@@ -82,9 +82,29 @@ class XFoilInterface:
         else:
             raise NameError('Unknown configuration {:s}'.format(self.configuration))
 
-        self.airfoil_data = read_excel(filepath)
+        for airfoil_name, airfoil_coords in read_excel(filepath).items():
+            self.add_airfoil(airfoil_name, airfoil_coords)
 
-    def run_angle_of_attack(self, alpha):
+    def add_airfoil(self, name, xy_tuple_or_str):
+        """
+
+        Args:
+            name (str): Airfoil name
+            x_coords (np.array): x-coordinates starting from TE
+            y_coords (np.array): y-coordinates starting from TE
+
+        """
+        self.airfoil_data[name] = xy_tuple_or_str
+
+    def reload_xfoil_airfoil(self, airfoil_entry, n_nodes=160):
+        if type(airfoil_entry) is str:
+            self.xfoil.naca(airfoil_entry)
+        else:
+            self.xfoil.airfoil = Airfoil(*airfoil_entry)
+        self.xfoil.repanel(n_nodes)
+        self.xfoil.reset_bls()
+
+    def run_angle_of_attack(self, alpha, **kwargs):
         """
 
         Args:
@@ -95,8 +115,7 @@ class XFoilInterface:
         """
 
         for airfoil_name, airfoil_coords in self.airfoil_data.items():
-            self.xfoil.airfoil = Airfoil(*airfoil_coords)
-            self.xfoil.repanel(n_nodes=250)
+            self.reload_xfoil_airfoil(airfoil_coords, kwargs.get('n_node', 160))
 
             cl, cd, cm, cp = self.xfoil.a(alpha)
             print('Airfoil', airfoil_name)
@@ -112,9 +131,7 @@ class XFoilInterface:
     def run_sequence_angle_of_attack(self, alpha_init, alpha_end, alpha_step, **kwargs):
         out_data = {}
         for airfoil_name, airfoil_coords in self.airfoil_data.items():
-            self.xfoil.airfoil = Airfoil(*airfoil_coords)
-            self.xfoil.repanel(n_nodes=250)
-            self.xfoil.Re = self.reynolds
+            self.reload_xfoil_airfoil(airfoil_coords, kwargs.get('n_node', 160))
 
             alpha_deg, cl, cd, cm, cp = self.xfoil.aseq(alpha_init, alpha_end, alpha_step)
             alpha_rad = alpha_deg * np.pi / 180
@@ -135,8 +152,7 @@ class XFoilInterface:
 
         out_data = {}
         for airfoil_name, airfoil_coords in self.airfoil_data.items():
-            self.xfoil.airfoil = Airfoil(*airfoil_coords)
-            self.xfoil.repanel(n_nodes=250)
+            self.reload_xfoil_airfoil(airfoil_coords, kwargs.get('n_node', 160))
 
             polar_data = np.zeros((n, 4))
             for i_alpha in range(n):
@@ -150,7 +166,7 @@ class XFoilInterface:
                 filename = self.output_directory + f'/xfoil_seq_re{self.reynolds:g}_{airfoil_name}.txt'
                 np.savetxt(filename,
                            out_data[airfoil_name],
-                           header='alpha_rad, cl, cd, cm')
+                           header='alpha_deg, cl, cd, cm')
                 print(f'Saved airfoil {airfoil_name} data to:\n\t{filename}')
 
         return out_data
@@ -185,10 +201,11 @@ class TestXFoil(unittest.TestCase):
         for name, coords in airfoil_coordinates.items():
             print(f'Airfoil {name}')
             self.xf.airfoil = Airfoil(coords[0], coords[1])
-            self.xf.repanel(n_nodes=250)
+            self.xf.repanel(n_nodes=300)
+            self.xf.reset_bls()
 
-            plt.plot(coords[0], coords[1], label='source', lw=0.5, color='k')
-            plt.plot(self.xf.airfoil.x, self.xf.airfoil.y, label='xfoil', lw=0.5, ls='--')
+            plt.plot(coords[0], coords[1], label=f'source {name}', lw=0.5, color='k')
+            plt.plot(self.xf.airfoil.x, self.xf.airfoil.y, label=f'xfoil {name}', lw=0.5, ls='--', ms=1, marker='o')
             plt.axis('equal')
             plt.legend()
             plt.savefig(f'./data_xfoil/airfoils_{name}.pdf')
@@ -196,11 +213,11 @@ class TestXFoil(unittest.TestCase):
 
     def test_run(self):
         xfi = XFoilInterface('Reference', save=False, output_directory='./data_xfoil/')
-        xfi.load_airfoils()
+        xfi.load_airfoils_from_excel()
         xfi.reynolds = 500000
 
         # out_data = xfi.run_sequence_angle_of_attack(-5, 5, 1, save=True)
-        out_data = xfi.run_polar(-3, 5, 1, save=True)
+        out_data = xfi.run_polar(-3, 5, 1, save=True, n_nodes=300)
         for airfoil_name, data in out_data.items():
             fig, ax = plot_polar(data[:, 0] * np.pi / 180, data[:, 1], data[:, 2], data[:, 3])
             fig.savefig(f'{xfi.output_directory}/polar{airfoil_name}.pdf')
@@ -210,9 +227,12 @@ class TestXFoil(unittest.TestCase):
         data = np.loadtxt(real_xfoil_file, skiprows=12)
         np.testing.assert_allclose(data[:, 0], out_data['root'][:, 0], err_msg='AoA not equal in both arrays. Check order')
 
-        np.testing.assert_allclose(data[:, 1], out_data['root'][:, 1], err_msg='Cl not equal to real xfoil', atol=1e-3)
-        np.testing.assert_allclose(data[:, 2], out_data['root'][:, 2], err_msg='Cd not equal to real xfoil', atol=1e-3)
-        np.testing.assert_allclose(data[:, 4], out_data['root'][:, 3], err_msg='Cm not equal to real xfoil', atol=1e-3)
+        np.testing.assert_allclose(data[:, 1], out_data['root'][:, 1], err_msg='Cl not equal to real xfoil',
+                                   atol=1e-3, rtol=1e-3)
+        np.testing.assert_allclose(data[:, 2], out_data['root'][:, 2], err_msg='Cd not equal to real xfoil',
+                                   atol=1e-3, rtol=1e-3)
+        np.testing.assert_allclose(data[:, 4], out_data['root'][:, 3], err_msg='Cm not equal to real xfoil',
+                                   atol=1e-3, rtol=1e-3)
 
 
 if __name__ == '__main__':
